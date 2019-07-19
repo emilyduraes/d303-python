986d7b783b7ed6e33c5393fec621d368f083a352
emilyduraes <emily.duraes@yahoo.com.br>
2019-07-19 11:24:39 -0300

    aprender orientação a objetos em Python

---
+++ main.py
@@ -0,0 +1,16 @@
+from classes import Carta, Baralho
+
+carta1 = Carta('Paus', 'As', 1)
+carta2 = Carta('Copas', 'Rei', 10)
+carta3 = Carta('Espadas', 'Cinco', 5)
+baralho = Baralho()
+
+print (baralho)
+
+baralho.adicionar_carta(carta1)
+baralho.adicionar_carta(carta2)
+baralho.adicionar_carta(carta3)
+baralho.remover_carta()
+
+print(baralho)
+print(baralho.remover_carta())
